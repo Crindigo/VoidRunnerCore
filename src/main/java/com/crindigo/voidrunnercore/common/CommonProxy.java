057481e7a0fcef3c8bb87b4b4c96f63f555bc697
Steven Harris <indigo@crindigo.com>
2024-09-02 21:20:26 -0400

    - added dummy materials to fix BQ crash, will remove if/when the modpack has custom cables/pipes

--- src/main/java/com/crindigo/voidrunnercore/common/CommonProxy.java
+++ src/main/java/com/crindigo/voidrunnercore/common/CommonProxy.java
@@ -4,11 +4,15 @@ import com.crindigo.voidrunnercore.Tags;
 import com.crindigo.voidrunnercore.VRCLog;
 import com.crindigo.voidrunnercore.common.item.VRCMetaItems;
 import com.crindigo.voidrunnercore.loaders.recipe.RecipeManager;
+import gregtech.api.GTValues;
 import gregtech.api.GregTechAPI;
 import gregtech.api.recipes.RecipeMaps;
+import gregtech.api.unification.material.Material;
 import gregtech.api.unification.material.event.MaterialRegistryEvent;
+import gregtech.api.unification.material.info.MaterialFlags;
 import net.minecraft.item.Item;
 import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -32,6 +36,23 @@ public class CommonProxy
     public static void registerMaterials(MaterialRegistryEvent event)
     {
         GregTechAPI.materialManager.createRegistry("voidrunner");
+
+        // seeing if registering a dummy material fixes a BlockCable getFallbackType() crash.
+        // there was a "voidrunner:cable_double" name and enabled materials was empty.
+
+        new Material.Builder(32765, new ResourceLocation("voidrunner", "dummy"))
+                .ingot()
+                .flags(MaterialFlags.GENERATE_FOIL) // will crash without this if you have a cable at IV+
+                .cableProperties(GTValues.V[GTValues.OpV], 16, 1)
+                .itemPipeProperties(1024, 64)
+                .color(0xff00ff)
+                .build();
+
+        new Material.Builder(32766, new ResourceLocation("voidrunner", "dummy_two"))
+                .ingot()
+                .fluidPipeProperties(65536, 4096, true, true, true, true)
+                .color(0xffff00)
+                .build();
     }
 
     @SubscribeEvent
